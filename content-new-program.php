97520163bec5de4078470c6622f65e0095cd996f
Stephen McCaskell <stephen.mccaskell@haventoday.org>
2018-02-06 11:07:57 -0600

    new program

---
+++ content-new-program.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: New Program
+ */
+
+get_header();
+get_template_part( 'template-parts/content', 'title' ); ?>
+
+<section class="c-padding-75">
+  <div class="container">
+      <script id="subsplash-embed-ghv4qb5" type="text/javascript">var target = document.getElementById("subsplash-embed-ghv4qb5");var script = document.createElement("script");script.type = "text/javascript";script.onload = function() {subsplashEmbed("+07a8/lb/ms/+ghv4qb5?embed&branding", "https://subsplash.com/", "subsplash-embed-ghv4qb5");};script.src = "https://dashboard.static.subsplash.com/production/web-client/external/embed-1.1.0.js";target.parentElement.insertBefore(script, target);</script>
+      <p>premiums</p>
+      <p>archives</p>
+      <p>haven now and anchor?</p>
+  </div>
+</section>
+
+<?php get_footer(); ?>
