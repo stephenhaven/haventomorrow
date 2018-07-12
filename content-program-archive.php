22ef4f3d64a4e4093a4da9ba2c09aa3bbd27426a
Stephen McCaskell <stephen.mccaskell@haventoday.org>
2018-07-11 22:31:05 -0500

    last bits

--- content-program-archive.php
+++ content-program-archive.php
@@ -8,7 +8,7 @@ get_template_part( 'template-parts/content', 'title' ); ?>
 
 <section class="c-padding-75">
   <div class="container">
-      Waiting for embed code from Subsplash.
+    <script id="subsplash-embed-pm2q2by" type="text/javascript">var target = document.getElementById("subsplash-embed-pm2q2by");var script = document.createElement("script");script.type = "text/javascript";script.onload = function() {subsplashEmbed("+07a8/lb/li/+pm2q2by?embed&branding", "https://subsplash.com/", "subsplash-embed-pm2q2by");};script.src = "https://dashboard.static.subsplash.com/production/web-client/external/embed-1.1.0.js";target.parentElement.insertBefore(script, target);</script>
   </div>
 </section>
 
--- template-parts/content-title.php
+++ template-parts/content-title.php
@@ -11,6 +11,6 @@
 
 <section class="c-padding-180 dark" style="background-image:url('<?php the_field('banner_image'); ?>')">
   <div class="container">
-    <h1 class="is-center"><?php wp_title('', true,''); ?></h1>
+    <h1 class="is-center"><?php the_title(''); ?></h1>
   </div>
 </section>
